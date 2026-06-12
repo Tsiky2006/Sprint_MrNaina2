c7c1a2b736bd63b406082e207df0697fa7fe8687
Achille Aubin <achilleaubinfanomezantsoa@gmail.com>
2026-06-12 10:28:16 +0200

    Servlet

--- src/com/sprint0/TestServlet.java
+++ src/com/sprint0/TestServlet.java
@@ -1,5 +1,30 @@
 package com.sprint0;
 
-public  {
-    
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class TestServlet extends HttpServlet {
+
+    protected void processRequest(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Sprint 0 OK</h1>");
+        out.println("URL : " + request.getRequestURL());
+    }
+
+    @Override
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        processRequest(request, response);
+    }
 }
